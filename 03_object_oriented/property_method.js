bc5d6eb88ddd0a4ae0e438c9824bafc2286d404d
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-10 00:07:10 +0900

    Create property_method.js

---
+++ 03_object_oriented/property_method.js
@@ -0,0 +1,10 @@
+// it's property
+var park_ji_sung = {
+    name : 'Park Ji Sung',
+    height : 178,
+    weight : 70,
+    position : 'RW',
+    team : 'Queen\'s Park Rangers'
+};
+
+console.log(park_ji_sung.team); // Queen's Park Rangers
